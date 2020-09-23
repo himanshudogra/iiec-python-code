32e1f8af1197607304f05d6100cf7992c718292f
himanshu <himanshudogra96@gmail.com>
2020-09-23 08:41:00 +0530

    Update dockerrun.py

--- docker/dockerrun.py
+++ docker/dockerrun.py
@@ -1,8 +1,13 @@
 import subprocess as sp
+import cgi
 
-input=input("Enter the container Name: ")
+#storing the HTML form's field info into form variable.
+form=cgi.FieldStorage()
 
-cmd=sp.getstatusoutput("sudo docker run -d -it --name {} ubuntu:latest".format(input))
+os_name=form.getvalue("x")
+os_image=form.getvalue("i")
+
+cmd=sp.getstatusoutput("sudo docker run -d -it --name {} {}".format(os_name,os_image))
 
 status=cmd[0]
 output=cmd[1]
@@ -11,7 +16,7 @@ output=cmd[1]
 
 if status == 0:
     print()
-    print("Container {} launched.".format(input))
+    print("Container {} launched.".format(os_name))
     print()
 else:
     print()
