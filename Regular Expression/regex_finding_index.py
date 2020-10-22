7fac4d6911397f02ae7c9e7a8801f90f76cbbea4
himanshu <himanshudogra96@gmail.com>
2020-10-22 09:08:26 +0530

    Update regex_finding_index.py

--- Regular Expression/regex_finding_index.py
+++ Regular Expression/regex_finding_index.py
@@ -6,6 +6,6 @@ logs='''
 62.75.198.179 - - [17/May/2015:08:05:06 +0000] "GET /downloads/product_2 HTTP/1.1" 302 490 "-" "Debian APT-HTTP/1.3 (0.9.7.9)"
 '''
 
-for i in finditer("^200",logs):
+for i in finditer("200",logs):
     loctuple=i.span()
     print(loctuple)
