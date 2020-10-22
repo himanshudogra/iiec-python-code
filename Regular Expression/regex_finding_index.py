e3599fe2e5e7fb14a763a9a316c6cc76f29a8877
himanshu <himanshudogra96@gmail.com>
2020-10-22 07:31:26 +0530

    Create regex_finding_index.py

---
+++ Regular Expression/regex_finding_index.py
@@ -0,0 +1,11 @@
+from re import *
+
+
+logs='''
+46.4.66.76 - - [17/May/2015:08:05:02 +0000] "GET /downloads/product_1 HTTP/1.1" 200 0 "-" "Debian APT-HTTP/1.3 (1.0.1ubuntu2)"
+62.75.198.179 - - [17/May/2015:08:05:06 +0000] "GET /downloads/product_2 HTTP/1.1" 302 490 "-" "Debian APT-HTTP/1.3 (0.9.7.9)"
+'''
+
+for i in finditer("^200",logs):
+    loctuple=i.span()
+    print(loctuple)
